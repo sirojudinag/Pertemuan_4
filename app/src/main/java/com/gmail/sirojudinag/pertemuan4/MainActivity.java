1086daec523e5f6cb6ab6a5424482636fdd53873
Sirojudin Abdul Ghopur <43234208+sirojudinag@users.noreply.github.com>
2019-04-02 08:43:15 +0700

    Update MainActivity.java

--- app/src/main/java/com/gmail/sirojudinag/pertemuan4/MainActivity.java
+++ app/src/main/java/com/gmail/sirojudinag/pertemuan4/MainActivity.java
@@ -7,10 +7,15 @@ import android.os.Bundle;
 //proses import package dari android bundle
 
 public class MainActivity extends AppCompatActivity {
+    //membuat class turunan dari main activity dengan nama AppCompatActivity
 
     @Override
+    //method overriding
     protected void onCreate(Bundle savedInstanceState) {
+        //Secara default, sistem menggunakan status instance Bundle untuk menyimpan informasi tentang setiap objek 
         super.onCreate(savedInstanceState);
+        //sistem sedang membuat instance baru aktivitas maupun membuat ulang instance sebelumnya.
         setContentView(R.layout.activity_main);
+         //Ini adalah metode Java yang disebut setContentView, yang akan menampilkan layout dari activity_main
     }
 }
